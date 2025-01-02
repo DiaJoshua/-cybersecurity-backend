9e4f7a9af004f71ed12a953deaea141536852f4d
DiaJoshua <diajoshua05@gmail.com>
2025-01-02 20:53:08 +0800

    Trying out Users for Database Configurations\

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -6,8 +6,8 @@ const bcrypt = require('bcrypt')
 // @route GET /users
 // @access Private
 const getAllUsers = asyncHandler(async (req, res) => {
-  const users = await user.find().select('-password').lean()
-  if (!users) {
+  const users = await User.find().select('-password').lean()
+  if (!users?.length) {
     return res.status(400).json({ message: 'No Users Found'})
   }
   res.json(users)

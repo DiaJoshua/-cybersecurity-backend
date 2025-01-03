19e60069b2ef9cc7817c072f21be9e3bc0f14d0b
DiaJoshua <diajoshua05@gmail.com>
2025-01-04 00:43:04 +0800

    Trying out Users for Database Configurations

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -20,7 +20,7 @@ const createNewUser = asyncHandler(async (req, res) => {
  const { username, password, roles } = req.body
 
  // Confirm data
- if (!username || !password || !Array.isArray(roles) || !roles.length) {
+ if (!username || !password) {
   return res.status(400).json({ message: 'All fields are required' })
  }
 
@@ -34,7 +34,7 @@ const createNewUser = asyncHandler(async (req, res) => {
   // Hash password
   const hashedPassword = await bcrypt.hash(password, 10) // salt rounds
 
-  const userObject = {username, "password": hashedPassword, roles}
+  const userObject = (!Array.isArray(roles) || !roles.length) ? { username, "password": hashedPassword } : { username, "password": hashedPassword, roles }
 
   // Create and Store new user
   const user = await User.create(userObject)

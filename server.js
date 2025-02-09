253a8275d5077ce831041674c40c883e78b5055d
DiaJoshua <diajoshua05@gmail.com>
2025-02-09 23:27:51 +0800

    Latest Update

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get('/fetch-cybercrime-news', async (req, res) => {
       return res.status(500).json({ error: 'API key is missing.' });
     }
 
-    const url = `https://newsapi.org/v2/everything?q=cybercrime+Philippines&language=en&sortBy=publishedAt&apiKey=${apiKey}`;
+    const url = `https://newsapi.org/v2/everything?q=cybercrime+Philippines&language=en&sortBy=publishedAt&apiKey=${process.env.NEWS_API_KEY}`;
     const response = await axios.get(url);
     
     if (!response.data.articles || response.data.articles.length === 0) {

a3d9245d3a5a40443cb80131ad5b052299589db8
DiaJoshua <diajoshua05@gmail.com>
2025-02-09 22:37:29 +0800

    Latest Update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,9 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5000',
   'https://clicksmart-backend.onrender.com',
-  'https://clicksmart-frontend.onrender.com'
+  'https://clicksmart-frontend.onrender.com',
+  'https://clicksmart-frontend.onrender.com/',
+  'https://clicksmart-backend.onrender.com/',
 ];
 
 module.exports = allowedOrigins;
